8dd881331bfb6d24f9ef65a4b962170146cf855b
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-08-24 20:18:48 +0800

    formated the terminal output

--- src/learn/chapter_01.clj
+++ src/learn/chapter_01.clj
@@ -2,35 +2,35 @@
 
 ; 练习 1.1
 
-(println
-  10                      ; 10
-  (+ 5 3 4)               ; 12
-  (- 9 1)                 ; 8
-  (/ 6 2)                 ; 3
-  (+ (* 2 4) (- 4 6)))    ; 6
+(println "practice 1.1-1:"
+         10                   ; 10
+         (+ 5 3 4)            ; 12
+         (- 9 1)              ; 8
+         (/ 6 2)              ; 3
+         (+ (* 2 4) (- 4 6))) ; 6
 
-(def a 3)                 ; 3
-(def b (+ a 1))           ; 4
+(def a 3)                     ; 3
+(def b (+ a 1))               ; 4
 
-(println
-  (+ a b (* a b))         ; 19
-  (= a b)
-  (if (and (> b a) (< b (* a b)))
-    b
-    a)                    ; 4
-  (cond (= a 4) 6
-        (= b 4) (+ 6 7 a)
-        (:else) 25)       ; 16
-  (+ 2 (if (> b a) b a))  ; 6
-  (* (cond (> a b) a
-           (< a b) b
-           (:else) -1)
-     (+ a 1))             ; 16
-  )
+(println "practice 1.1-2:"
+         (+ a b (* a b))        ; 19
+         (= a b)
+         (if (and (> b a) (< b (* a b)))
+           b
+           a)                   ; 4
+         (cond (= a 4) 6
+               (= b 4) (+ 6 7 a)
+               (:else) 25)      ; 16
+         (+ 2 (if (> b a) b a)) ; 6
+         (* (cond (> a b) a
+                  (< a b) b
+                  (:else) -1)
+            (+ a 1)))           ; 16
 
 ; 练习 1.2
 
-(println (/ (+ 5
+(println "practice 1.2:"
+         (/ (+ 5
                4
                (- 2
                   (- 3
@@ -49,7 +49,8 @@
          (cond (and (> a c) (> b c)) (+ a b)
                (and (> c b) (> a b)) (+ c a)
                :else (+ b c))))
-(println (merge-bigger-1 1 2 3))
+(println "practice 1.3-1:"
+         (merge-bigger-1 1 2 3))
 
 (def bigger
      (fn [a b]
@@ -68,4 +69,26 @@
          (+ (bigger a b)
             (bigger (smaller a b) c))))
 
-(println (merge-bigger-2 1 2 3))
+(println "practice 1.3-2:"
+         (merge-bigger-2 1 2 3))
+
+; 练习 1.4
+
+; 类 lisp 语言中, 函数也是一种变量
+; 可以作为参数传递, 从参数中获得的函数也可以直接拿来调用
+
+(def a-plus-abs-b
+     (fn [a b]
+         ((if (> b 0)
+              +
+              -) a
+                 b)))
+
+(println "practice 1.4:"
+         (a-plus-abs-b 1 2)
+         (a-plus-abs-b 1 -2))
+
+; 练习 1.5
+
+(defn p [] p)
+(println (p))
